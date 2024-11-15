3eb1d0e3ba5d0eb31faf8cb30ec2b78c3b4989a6
axl6409 <alexandre.celier64@gmail.com>
2024-11-15 09:35:08 +0100

    First commit

---
+++ eco-grader.php
@@ -0,0 +1,36 @@
+<div class="ecograder__container ecograder__light"> <!-- Use ecograder__dark for dark theme -->
+    <div class="ecograder__inner-top">
+        <ul class="ecograder__list">
+            <li class="ecograder__list-item">
+                <p>A+</p>
+            </li>
+            <li class="ecograder__list-item ecograder__list-item--active">
+                <p>A</p>
+            </li>
+            <li class="ecograder__list-item">
+                <p>B</p>
+            </li>
+            <li class="ecograder__list-item">
+                <p>C</p>
+            </li>
+            <li class="ecograder__list-item">
+                <p>D</p>
+            </li>
+            <li class="ecograder__list-item">
+                <p>E</p>
+            </li>
+            <li class="ecograder__list-item">
+                <p>F</p>
+            </li>
+        </ul>
+    </div>
+    <div class="ecograder__inner-divider"></div>
+    <div class="ecograder__inner-bottom">
+        <div class="ecograder__bottom-left">
+            <a href="https://www.websitecarbon.com/website/burographic-fr/" target="_blank">>0,17G de CO2/Vue</a>
+        </div>
+        <div class="ecograder__bottom-right">
+            <a href="https://www.websitecarbon.com/" target="_blank">Website carbon</a>
+        </div>
+    </div>
+</div>
